23a0552aee15543935e163f940eba615e785c97e
OracleTM <oracletm@yandex.ru>
2025-09-03 20:35:20 +0800

    Add logger and Allure, rename same methods

---
+++ utilities/logger.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+import os
+from pathlib import Path
+
+
+class Logger():
+
+
+    base_dir = Path(__file__).parent.parent
+    log_dir = base_dir / "logs"
+    log_dir.mkdir(exist_ok=True)
+    file_name = log_dir / f"log_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+
+
+
+    @classmethod
+    def write_log_to_file(cls, data: str):
+        with open(cls.file_name, 'a', encoding='utf=8') as logger_file:
+            logger_file.write(data)
+
+    @classmethod
+    def add_start_step(cls, method: str):
+        test_name = os.environ.get('PYTEST_CURRENT_TEST')
+
+        data_to_add = f"\n-----\n"
+        data_to_add += f"Test: {test_name}\n"
+        data_to_add += f"Start time: {str(datetime.now())}\n"
+        data_to_add += f"Start name method: {method}\n"
+        data_to_add += "\n"
+
+        cls.write_log_to_file(data_to_add)
+
+    @classmethod
+    def add_end_step(cls, url: str, method: str):
+
+        data_to_add = f"End time: {str(datetime.now())}\n"
+        data_to_add += f"End name method: {method}\n"
+        data_to_add += f"URL: {url}\n"
+        data_to_add += f"\n-----\n"
+
+        cls.write_log_to_file(data_to_add)
